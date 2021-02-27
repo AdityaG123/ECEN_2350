484050f5f5886fa9009bd844397cfc136d25550e
AdityaG123 <45723751+AdityaG123@users.noreply.github.com>
2021-02-27 16:16:50 -0700

    Delete Lab 1_Venners_Gopalan/Lab 1/Integrated directory

--- Lab 1_Venners_Gopalan/Lab 1/Integrated/Verilog/full_adder.v
+++
@@ -1,6 +0,0 @@
-module full_adder(Cin,a,b,sum,Cout);
-input Cin,a,b;
-output wire sum, Cout;
-assign sum = Cin ^ a ^ b; //if two 1s sum = 0, if 3 1s sum = 1, if only 1 1 then sum = 1
-assign Cout = (a & b) | (a & Cin) | (b & Cin); //if there are atleast two ones in inputs
-endmodule
